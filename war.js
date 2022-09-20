7633622cc55734e02e6d61732f5b2d05a1a1836c
Andrea Alonso <andreaalonso@andreas-air.myfiosgateway.com>
2022-09-20 00:38:40 -0400

    added logic to play the game

--- war.js
+++ war.js
@@ -1,36 +1,36 @@
 let playerOne = {
   cardsPlayed: [],
-  roundWon: false,
   numCards: [],
 }
 
 let playerTwo = {
   cardsPlayed: [],
-  roundWon: false,
   numCards: [],
 }
 
+let roundWon = "";
+
 let deck = [
-  2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A",
-  2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"
+  "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
+  "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
+  "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A",
+  "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"
 ]
 
 let faceValue = {
-  2: 2,
-  3: 3,
-  4: 4,
-  5: 5,
-  6: 6,
-  7: 7,
-  8: 8,
-  9: 9,
-  10: 10,
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "10": 10,
   "J": 11,
   "Q": 12,
   "K": 13,
-  "A": 14,
+  "A": 14
 }
 
 function shuffleTheDeck(arr) {
@@ -54,3 +54,73 @@ for (let i = 0; i < 26; i++) {
 
 console.log(playerOne.numCards);
 console.log(playerTwo.numCards);
+
+let roundNumber = 1;
+while (playerOne.numCards.length != 52 && playerTwo.numCards.length != 52) {
+  playerOne.cardsPlayed = [];
+  playerOne.cardsPlayed.push(playerOne.numCards.shift());
+
+  playerTwo.cardsPlayed = [];
+  playerTwo.cardsPlayed.push(playerTwo.numCards.shift());
+
+  if (faceValue[playerOne.cardsPlayed[0]] > faceValue[playerTwo.cardsPlayed[0]]) {
+    roundWon = "PlayerOne";
+    playerOne.numCards.push(playerOne.cardsPlayed[0]);
+    playerOne.numCards.push(playerTwo.cardsPlayed[0]);
+  }
+  else if (faceValue[playerTwo.cardsPlayed[0]] > faceValue[playerOne.cardsPlayed[0]]) {
+    roundWon = "PlayerTwo";
+    playerTwo.numCards.push(playerOne.cardsPlayed[0]);
+    playerTwo.numCards.push(playerTwo.cardsPlayed[0]);
+  }
+  else {
+    roundWon = "tie";
+    while (roundWon == "tie") {
+      if (playerOne.numCards.length > 0) {
+        playerOne.cardsPlayed.push(playerOne.numCards.shift());
+      }
+      if (playerOne.numCards.length > 0) {
+        playerOne.cardsPlayed.push(playerOne.numCards.shift());
+      }
+      if (playerOne.numCards.length > 0) {
+        playerOne.cardsPlayed.push(playerOne.numCards.shift());
+      }
+      if (playerOne.numCards.length > 0) {
+        playerOne.cardsPlayed.push(playerOne.numCards.shift());
+      }
+
+      if (playerTwo.numCards.length > 0) {
+        playerTwo.cardsPlayed.push(playerTwo.numCards.shift());
+      }
+      if (playerTwo.numCards.length > 0) {
+        playerTwo.cardsPlayed.push(playerTwo.numCards.shift());
+      }
+      if (playerTwo.numCards.length > 0) {
+        playerTwo.cardsPlayed.push(playerTwo.numCards.shift());
+      }
+      if (playerTwo.numCards.length > 0) {
+        playerTwo.cardsPlayed.push(playerTwo.numCards.shift());
+      }
+
+      if (faceValue[playerOne.cardsPlayed[playerOne.cardsPlayed.length - 1]] > 
+        faceValue[playerTwo.cardsPlayed[playerTwo.cardsPlayed.length - 1]]) {
+          roundWon = "PlayerOne";
+          playerOne.numCards = playerOne.numCards.concat(playerOne.cardsPlayed);
+          playerOne.numCards = playerOne.numCards.concat(playerTwo.cardsPlayed);
+      }
+      else if (faceValue[playerTwo.cardsPlayed[playerTwo.cardsPlayed.length - 1]] > 
+        faceValue[playerOne.cardsPlayed[playerOne.cardsPlayed.length - 1]]) {
+          roundWon = "PlayerTwo";
+          playerTwo.numCards = playerTwo.numCards.concat(playerTwo.cardsPlayed);
+          playerTwo.numCards = playerTwo.numCards.concat(playerOne.cardsPlayed);
+      }
+    }
+  }
+  console.log("Round: " + roundNumber);
+  console.log("PlayerOne cards played: " + playerOne.cardsPlayed);
+  console.log("PlayerTwo cards played: " + playerTwo.cardsPlayed);
+  console.log(roundWon + " is winner!");
+  console.log("PlayerOne number of cards = " + playerOne.numCards);
+  console.log("PlayerTwo number of cards = " + playerTwo.numCards);
+  roundNumber++;
+}
